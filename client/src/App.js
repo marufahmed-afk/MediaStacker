1cc449fdb9cc70bc54aa865e636aeca2d1d026ea
samdi18 <41270492+samdi18@users.noreply.github.com>
2020-05-18 22:31:23 +0600

    auth created

--- client/src/App.js
+++ client/src/App.js
@@ -3,25 +3,32 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Home from "./components/pages/Home";
 import Comics from "./components/comics/Comics";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 
 import ComicState from "./context/comic/ComicState";
 import "./App.css";
+import AuthState from "./context/auth/AuthState";
 
 const App = () => {
   return (
-    <ComicState>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/comics" component={Comics} />
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    </ComicState>
+    <AuthState>
+      <ComicState>
+        <Router>
+          <Fragment>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/comics" component={Comics} />
+                <Route exact path="/register" component={Register} />
+                <Route exact path="/login" component={Login} />
+              </Switch>
+            </div>
+          </Fragment>
+        </Router>
+      </ComicState>
+    </AuthState>
   );
 };
 
